864b0f788244f11582f1df20bd21dbf5fa4b6836
SakuyaYae <darkreimu@outlook.com>
2024-10-15 11:30:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import CompareHands from './CompareHands.js';
 // ♥♦♣♠
 
 let hand1 = new Hand('♠9', '♦9', '♣9', '♥9', '♥K');
-let hand2 = new Hand('♣5', '♣T', '♣J', '♦Q', '♣K');
+let hand2 = new Hand('♣5', '♦J', '♣J', '♦5', '♣K');
 //console.log(CompareHands.comparer(hand1, hand2));
 console.log(CompareHands.isThreeOfAKind(hand1));
-
+console.log(CompareHands.isFourOfAKind(hand1));
+console.log(CompareHands.isOnePair(hand2));
+console.log(CompareHands.isTwoPair(hand2));
