339aea2d0b1d1b5f3ca2bb5b61cbf29b92e0209e
SakuyaYae <darkreimu@outlook.com>
2024-10-16 10:59:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import CompareHands from './CompareHands.js';
 
 // ♥♦♣♠
 
-let hand1 = new Hand('♠9', '♦9', '♣9', '♥9', '♥K');
-let hand2 = new Hand('♣5', '♦J', '♣J', '♦5', '♣K');
+let hand1 = new Hand('♥K', '♦9', '♣9', '♥9', '♠9');
+let hand2 = new Hand('♣4', '♦9', '♠8', '♦3', '♣7');
 //console.log(CompareHands.comparer(hand1, hand2));
 console.log(CompareHands.isThreeOfAKind(hand1));
 console.log(CompareHands.isFourOfAKind(hand1));
-console.log(CompareHands.isOnePair(hand2));
-console.log(CompareHands.isTwoPair(hand2));
+console.log("one pair: " + CompareHands.isOnePair(hand2));
+//console.log("Two pair: " + CompareHands.isTwoPair(hand2));
+//console.log("Fullhouse: " + CompareHands.isFullHouse(hand2));
+console.log(CompareHands.isHighestCard(hand2));
