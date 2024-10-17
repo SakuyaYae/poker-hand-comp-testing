52ffcb6d4f84141dae82816f40f2d54f830e5dbb
SakuyaYae <darkreimu@outlook.com>
2024-10-17 11:02:44 +0200

    Update isFourOfAKind.test.js

--- tests/isFourOfAKind.test.js
+++ tests/isFourOfAKind.test.js
@@ -1,8 +1,12 @@
 import { expect, test } from 'vitest';
 import Hand from '../Hand.js';
-import CompareHands from '../CompareHands.js';const suits = '♥♦♣♠';
+import CompareHands from '../CompareHands.js'; const suits = '♥♦♣♠';
 
 test('Test that fourOfAKind returns truthy if hand holds four of a kind', () => {
-  let hand = new Hand('♠7', '♥2', '♦2', '♣2', '♠2' );
+  let hand = new Hand('♠7', '♥2', '♦2', '♣2', '♠2');
   expect(CompareHands.isFourOfAKind(hand)).toBeTruthy();
+});
+test('Test that fourOfAKind returns Falsy if hand does not hold holds four of a kind', () => {
+  let hand = new Hand('♠2', '♥2', '♦3', '♣T', '♥4');
+  expect(CompareHands.isFourOfAKind(hand)).toBeFalsy();
 });
