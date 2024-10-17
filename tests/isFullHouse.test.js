09066f12679588000dc8343793426063f68f0b0f
SakuyaYae <darkreimu@outlook.com>
2024-10-17 11:02:47 +0200

    Update isFullHouse.test.js

--- tests/isFullHouse.test.js
+++ tests/isFullHouse.test.js
@@ -1,8 +1,13 @@
 import { expect, test } from 'vitest';
 import Hand from '../Hand.js';
-import CompareHands from '../CompareHands.js';const suits = '♥♦♣♠';
+import CompareHands from '../CompareHands.js'; const suits = '♥♦♣♠';
 
 test('Test that isFullHouse returns truthy if hand holds 3 of a kind and 2 of a kind', () => {
-  let hand = new Hand('♠7', '♥6', '♦6', '♣7', '♠6' );
+  let hand = new Hand('♠8', '♥7', '♦8', '♣7', '♥8');
   expect(CompareHands.isFullHouse(hand)).toBeTruthy();
+});
+
+test('Test that isFullHouse returns Falsy if hand does not hold 3 of a kind and 2 of a kind', () => {
+  let hand = new Hand('♠2', '♥7', '♦3', '♣7', '♥4');
+  expect(CompareHands.isFullHouse(hand)).toBeFalsy();
 });
