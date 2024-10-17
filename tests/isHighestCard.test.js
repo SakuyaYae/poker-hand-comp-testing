af99352530f2cc652f40108aaaf8b127d4fa0d0a
SakuyaYae <darkreimu@outlook.com>
2024-10-17 11:02:49 +0200

    Update isHighestCard.test.js

--- tests/isHighestCard.test.js
+++ tests/isHighestCard.test.js
@@ -5,14 +5,17 @@ import CompareHands from '../CompareHands.js';
 //const suits = '♥♦♣♠';
 
 
-test('check that isHighestCard returns the highst card for the stronger hand', () => {
-  let hand1 = new Hand('♣2', '♣6', '♣4', '♣8', '♣7');
-  let hand2 = new Hand('♦9', '♦8', '♦5', '♦6', '♦3');
-  let hand1Highest = CompareHands.isHighestCard(hand1);
-  let hand2Highest = CompareHands.isHighestCard(hand2);
-  console.log(hand1Highest)
-  console.log(hand2Highest)
-  
-  //expect(hand2Highest).toBeGreaterThan(hand1Highest);
 
-  });
+test('check that isHighestCard returns the highst card for the stronger hand', () => {
+  let hand1 = new Hand('♣2', '♣6', '♠4', '♣8', '♣7');
+  let hand2 = new Hand('♣4', '♦Q', '♠T', '♦4', '♣9');
+  expect(CompareHands.rankToPoint(CompareHands.isHighestCard(hand2).rank)).toBeGreaterThan(CompareHands.rankToPoint(CompareHands.isHighestCard(hand1).rank));
+
+});
+
+test('check that isHighestCard returns the highst card for the stronger hand', () => {
+  let hand1 = new Hand('♣2', '♣A', '♠4', '♣8', '♣7');
+  let hand2 = new Hand('♣4', '♦Q', '♠T', '♦4', '♣9');
+  expect(CompareHands.rankToPoint(CompareHands.isHighestCard(hand1).rank)).toBeGreaterThan(CompareHands.rankToPoint(CompareHands.isHighestCard(hand2).rank));
+
+});
